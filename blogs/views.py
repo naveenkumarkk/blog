5e2f3280692b5d6767e20b6b32c2c9d2fee966b5
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-12-20 21:02:25 +0200

    feat: dynamic posts
    Added logic to fetch posts and feature articles contents from DB

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
+
 
 # Create your views here.
+def posts_by_category(request, category_id):
+    # Fetch the posts that belongs to the category with the id category_id
+    posts = Blog.objects.filter(status='Published',category_id=category_id)
+    return HttpResponse("Posts By Category")
