5c081141e995500e23c4ded24b7fdcb83187d63b
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-12-23 12:23:12 +0200

    feat: login and logout
    Added Login , register, logout pages and its functionality

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogs'
+    'blogs',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
