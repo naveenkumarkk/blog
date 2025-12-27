5b34266ce5ef03b64515e021f5235874f45d3d67
Naveen Kumar <42708813+naveenkumarkk@users.noreply.github.com>
2025-12-27 23:51:52 +0200

    Update settings.py

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'blog_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
